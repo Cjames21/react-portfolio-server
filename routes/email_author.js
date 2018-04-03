b3d7b53af5b69c8f7d7191d420638e1743252e01
Charles James <charlestdjames1@gmail.com>
2018-04-03 18:59:47 -0400

    Clean slate for setting up back end that I acctually need
    The express generator created a lot of things that my website doesn't need. (React is handling the views in the client folder). I deleted the entire back end, now it is time to rebuild.

--- routes/email_author.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.post('/send_mail', function(req, res) {
-  console.log(req);
-  res.send(req);
-});
-
-module.exports = router;
